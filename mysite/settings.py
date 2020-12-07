f79bd7f8ebc2d113dba0f9ec3818a781ec08982f
enomoto-r <enomoto138aws@gmailcom>
2020-12-07 13:54:58 +0900

    update allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9tcwc5h(80%^ahfja##x((@-5)#t4&$^i+zgk-_d3=cb)$#qs&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
